b38d21f08a073dd4e8911679f8e73c4ff0d329ed
kreetz <bronze.reach@gmail.com>
2025-01-13 15:15:45 +0200

    first commit

---
+++ Assigment_1.1/src/Task_2.java
@@ -0,0 +1,11 @@
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+        String star = "    *    ";
+        System.out.println(star.length());
+        for (int i = 0; i < 4; i++) {
+            System.out.println(star);
+        }
+    }
+}
