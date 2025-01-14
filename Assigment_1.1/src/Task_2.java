7debee16377929078e559af7861fbdebf2531583
kreetz <bronze.reach@gmail.com>
2025-01-14 11:01:56 +0200

    task 2

--- Assigment_1.1/src/Task_2.java
+++ Assigment_1.1/src/Task_2.java
@@ -2,10 +2,13 @@
 public class Task_2 {
 
     public static void main(String[] args) {
-        String star = "    *    ";
-        System.out.println(star.length());
+        String star = "*";
+        int space = 5;
         for (int i = 0; i < 4; i++) {
-            System.out.println(star);
+            String formatted = String.format("%" + space + "s", star);
+            System.out.println(formatted);
+            star = star + "**";
+            space++;
         }
     }
 }
